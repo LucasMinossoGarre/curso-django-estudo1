eec1002338d29a93a1778e0671624f8b752db0ae
Lucas Garre <124816375+LucasMinossoGarre@users.noreply.github.com>
2024-06-30 07:32:50 -0300

    admin-config

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -21,3 +24,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+
+    def __str__(self) -> str:
+        return self.title
