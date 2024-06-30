cddc57243945badb45880dc290944dae983f49af
Lucas Garre <124816375+LucasMinossoGarre@users.noreply.github.com>
2024-06-30 07:54:43 -0300

    category-id-url

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 
+
+
 urlpatterns = [
     path('', views.home, name='recipes-home'),
+    path('recipes/category/<int:category_id>/', views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
 ]
